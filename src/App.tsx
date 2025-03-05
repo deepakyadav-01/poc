63b4ffe9d38f0fc4b2a67c76a21f3536abb1224e
Deepak Yadav <mfsi.deepak.y@gmail.com>
2025-03-05 17:41:15 +0530

    changes new

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,55 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import SelectShowcase from './components/select/SelectShowcase'
+import TextareaShowcase from './components/textarea/TextareaShowcase'
+import RadioShowcase from './components/radio/RadioShowcase'
+import InputShowcase from "./components/input/InputShowcase";
 
-import Input from "./pages/input"
-
-const RouteFile: React.FC = () => {
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Input />} />
-      </Routes>
+      <div className="min-h-screen bg-gray-100">
+        <nav className="bg-white shadow-md">
+          <div className="max-w-6xl mx-auto px-4">
+            <div className="flex justify-center space-x-8 py-4">
+            <Link 
+                to="/input" 
+                className="text-gray-600 hover:text-blue-500 transition-colors"
+              >
+                Input Animation
+              </Link>
+              <Link 
+                to="/select" 
+                className="text-gray-600 hover:text-blue-500 transition-colors"
+              >
+                Select Animations
+              </Link>
+              <Link 
+                to="/textarea" 
+                className="text-gray-600 hover:text-blue-500 transition-colors"
+              >
+                Textarea Animations
+              </Link>
+              <Link 
+                to="/radio" 
+                className="text-gray-600 hover:text-blue-500 transition-colors"
+              >
+                Radio Animations
+              </Link>
+            </div>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/select" element={<SelectShowcase />} />
+          <Route path="/input" element={<InputShowcase />} />
+          <Route path="/textarea" element={<TextareaShowcase />} />
+          <Route path="/radio" element={<RadioShowcase />} />
+          <Route path="/" element={<InputShowcase />} />
+        </Routes>
+      </div>
     </Router>
   );
-};
+}
 
-export default RouteFile;
+export default App;
